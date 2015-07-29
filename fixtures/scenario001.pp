60b54ff291412c81d7d1c57f1a54f93e81ef6d74
Emilien Macchi <emilien@redhat.com>
2015-07-29 10:28:04 -0400

    scenario001: deploy MySQL and Keystone (WSGI)
    Deploy MySQL and Keystone in a WSGI process with Apache, like it's done
    in current beaker jobs.
    
    Change-Id: Ib54e90150c8e98d5cf028766992db491920ca028

--- fixtures/scenario001.pp
+++ fixtures/scenario001.pp
@@ -14,4 +14,68 @@
 # limitations under the License.
 #
 
-notice('Successful Puppet run')
+Exec { logoutput => 'on_failure' }
+
+# Common resources
+case $::osfamily {
+  'Debian': {
+    include ::apt
+    apt::ppa { 'ppa:ubuntu-cloud-archive/liberty-staging':
+      # it's false by default in 2.x series but true in 1.8.x
+      package_manage => false,
+    }
+    Exec['apt_update'] -> Package<||>
+  }
+  'RedHat': {
+    class { '::openstack_extras::repo::redhat::redhat':
+      manage_rdo => false,
+      repo_hash  => {
+        # we need kilo repo to be installed for dependencies
+        'rdo-kilo'    => {
+          'baseurl'  => 'https://repos.fedorapeople.org/repos/openstack/openstack-kilo/el7/',
+          'descr'    => 'RDO kilo',
+          'gpgcheck' => 'no',
+        },
+        'rdo-liberty' => {
+          'baseurl'  => 'http://trunk.rdoproject.org/centos7/current/',
+          'descr'    => 'RDO trunk',
+          'gpgcheck' => 'no',
+        },
+      },
+    }
+    package { 'openstack-selinux': ensure => 'latest' }
+  }
+  default: {
+    fail("Unsupported osfamily (${::osfamily})")
+  }
+}
+
+# Deploy MySQL Server
+class { '::mysql::server': }
+
+# Deploy Keystone
+class { '::keystone::client': }
+class { '::keystone::cron::token_flush': }
+class { '::keystone::db::mysql':
+  password => 'keystone',
+}
+class { '::keystone':
+  verbose             => true,
+  debug               => true,
+  database_connection => 'mysql://keystone:keystone@127.0.0.1/keystone',
+  admin_token         => 'admin_token',
+  enabled             => true,
+  service_name        => 'httpd',
+  default_domain      => 'default_domain',
+}
+include ::apache
+class { '::keystone::wsgi::apache':
+  ssl => false,
+}
+class { '::keystone::roles::admin':
+  email    => 'test@example.tld',
+  password => 'a_big_secret',
+}
+class { '::keystone::endpoint':
+  default_domain => 'admin',
+}
