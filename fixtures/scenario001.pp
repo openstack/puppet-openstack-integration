6f673f0d7903ce3f04892dd2f646c58c0a3ec0cd
Emilien Macchi <emilien@redhat.com>
2015-08-25 23:14:34 -0400

    scenario001: deploy Nova
    This patch deploy Nova:
    * RabbitMQ, MySQL and Keystone resources
    * Common nova service bits
    * API, scheduler, conductor, console, compute (with qemu), cert, client,
      consoleauth, vncpoxy, neutron parameters
    * Neutron configuration to talk to Nova API
    * neutron_metadata_proxy_shared_secret parameter to talk to neutron
      metadata proxy
    
    Change-Id: I9d46ba69df624a372abb84e22b1e8e28c84e23a7

--- fixtures/scenario001.pp
+++ fixtures/scenario001.pp
@@ -64,6 +64,19 @@ rabbitmq_vhost { '/':
   provider => 'rabbitmqctl',
   require  => Class['rabbitmq'],
 }
+rabbitmq_user { ['neutron', 'nova']:
+  admin    => true,
+  password => 'an_even_bigger_secret',
+  provider => 'rabbitmqctl',
+  require  => Class['rabbitmq'],
+}
+rabbitmq_user_permissions { ['neutron@/', 'nova@/']:
+  configure_permission => '.*',
+  write_permission     => '.*',
+  read_permission      => '.*',
+  provider             => 'rabbitmqctl',
+  require              => Class['rabbitmq'],
+}
 
 # Deploy Keystone
 class { '::keystone::client': }
@@ -129,19 +142,6 @@ glance_image { 'cirros_alt':
 }
 
 # Deploy Neutron
-rabbitmq_user { 'neutron':
-  admin    => true,
-  password => 'an_even_bigger_secret',
-  provider => 'rabbitmqctl',
-  require  => Class['rabbitmq'],
-}
-rabbitmq_user_permissions { 'neutron@/':
-  configure_permission => '.*',
-  write_permission     => '.*',
-  read_permission      => '.*',
-  provider             => 'rabbitmqctl',
-  require              => Class['rabbitmq'],
-}
 class { '::neutron::db::mysql':
   password => 'neutron',
 }
@@ -191,6 +191,49 @@ class { '::neutron::agents::dhcp':
 }
 class { '::neutron::agents::metering':
   debug => true,
+}
+class { '::neutron::server::notifications':
+  nova_admin_password => 'a_big_secret',
+}
+
+# Deploy Nova
+class { '::nova::db::mysql':
+  password => 'nova',
+}
+class { '::nova::keystone::auth':
+  password => 'a_big_secret',
+}
+class { '::nova':
+  database_connection => 'mysql://nova:nova@127.0.0.1/nova?charset=utf8',
+  rabbit_host         => '127.0.0.1',
+  rabbit_userid       => 'nova',
+  rabbit_password     => 'an_even_bigger_secret',
+  glance_api_servers  => 'localhost:9292',
+  verbose             => true,
+  debug               => true,
+}
+class { '::nova::api':
+  admin_password                       => 'a_big_secret',
+  identity_uri                         => 'http://127.0.0.1:35357/',
+  osapi_v3                             => true,
+  neutron_metadata_proxy_shared_secret => 'a_big_secret',
+}
+class { '::nova::cert': }
+class { '::nova::client': }
+class { '::nova::conductor': }
+class { '::nova::consoleauth': }
+class { '::nova::cron::archive_deleted_rows': }
+class { '::nova::compute': vnc_enabled => true }
+class { '::nova::compute::libvirt':
+  libvirt_virt_type => 'qemu',
+  migration_support => true,
+  vncserver_listen  => '0.0.0.0',
+}
+class { '::nova::scheduler': }
+class { '::nova::vncproxy': }
+class { '::nova::network::neutron':
+  neutron_admin_password => 'a_big_secret',
+  neutron_admin_auth_url => 'http://127.0.0.1:35357/v2.0',
 }
 
 class { '::tempest':
