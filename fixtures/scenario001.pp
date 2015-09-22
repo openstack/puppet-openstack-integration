d4a358aca91692fe50d0a29cd561231d7af2fa35
Emilien Macchi <emilien@redhat.com>
2015-09-22 13:47:53 -0400

    Bump RDO packages to tested repos
    RDO has now [1] a repo that is tested against real CI.
    Using this repository will avoid more failures in our CI.
    
    [1] http://trunk.rdoproject.org/centos7-liberty/current-passed-ci/
    
    Change-Id: I46821aa3621745596aff572ad50bd47674f6ccb7

--- fixtures/scenario001.pp
+++ fixtures/scenario001.pp
@@ -42,7 +42,7 @@ case $::osfamily {
           'gpgcheck' => 'no',
         },
         'openstack-liberty-trunk'   => {
-          'baseurl'  => 'http://trunk.rdoproject.org/centos7-liberty/current/',
+          'baseurl'  => 'http://trunk.rdoproject.org/centos7-liberty/current-passed-ci/',
           'descr'    => 'openstack-liberty-trunk',
           'gpgcheck' => 'no',
         },
