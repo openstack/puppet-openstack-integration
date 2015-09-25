67c05c86cb46e20b624a74bd5c7d428f2964d73d
Denis Egorenko <degorenko@mirantis.com>
2015-09-25 12:29:24 +0300

    scenario001: enable data processing service
    * Install & configure Sahara services
    * Enable telemetry tests in Tempest
    
    Change-Id: I8934ee620096fadc20897cb0fd95a68492bcff2f
    Depends-On: Ic4054404a4683a6faf766eca02cf1c0c846d57c2

--- run_tests.sh
+++ run_tests.sh
@@ -84,5 +84,5 @@ fi
 # TODO(emilien) later, we should use local image if present. That would be a next iteration.
 wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img -P /tmp/openstack/tempest
 
-# run a scenario that validates Keystone, Nova, Glance, Neutron, Ceilometer and Cinder
+# run a scenario that validates Keystone, Nova, Glance, Neutron, Ceilometer, Cinder and Sahara
 cd /tmp/openstack/tempest; tox -eall -- --concurrency=4 smoke
