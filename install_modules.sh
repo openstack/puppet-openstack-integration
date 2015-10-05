acb145de3298cd4f2c27d7fd849e56ba527fe023
Emilien Macchi <emilien@redhat.com>
2015-10-05 16:39:26 -0400

    Create functions for common bits
    * Move out some code from install_modules.sh to have a functions script.
    * Update install_modules.sh script to use functions.
    
    Change-Id: Ie87b8eac71d5edb555e9a1637bea84bc9e268497

--- install_modules.sh
+++ install_modules.sh
@@ -8,47 +8,13 @@ fi
 
 export SCRIPT_DIR=$(readlink -f "$(dirname $0)")
 export PUPPETFILE_DIR=${PUPPETFILE_DIR:-/etc/puppet/modules}
-
-install_external() {
-  PUPPETFILE=${SCRIPT_DIR}/Puppetfile1 ${GEM_BIN_DIR}r10k puppetfile install -v
-}
-
-install_openstack() {
-  cat > clonemap.yaml <<EOF
-clonemap:
-  - name: '(.*?)/puppet-(.*)'
-    dest: '$PUPPETFILE_DIR/\2'
-EOF
-
-  local project_names=$(awk '{ if ($1 == ":git") print $3 }' \
-    ${SCRIPT_DIR}/Puppetfile0 | tr -d "'," | cut -d '/' -f 4- | xargs
-  )
-  /usr/zuul-env/bin/zuul-cloner -m clonemap.yaml \
-    --cache-dir /opt/git \
-    --zuul-ref $ZUUL_REF \
-    --zuul-branch $ZUUL_BRANCH \
-    --zuul-url $ZUUL_URL \
-    git://git.openstack.org $project_names
-}
-
-install_all() {
-  PUPPETFILE=${SCRIPT_DIR}/Puppetfile ${GEM_BIN_DIR}r10k puppetfile install -v
-}
+source $SCRIPT_DIR/functions
 
 gem install r10k --no-ri --no-rdoc
 
 # make sure there is no puppet module pre-installed
 rm -rf "${PUPPETFILE_DIR:?}/"*
 
-# If zuul-cloner is there, have it install modules using zuul refs
-if [ -e /usr/zuul-env/bin/zuul-cloner ] ; then
-  csplit ${SCRIPT_DIR}/Puppetfile /'External modules'/ \
-    --prefix ${SCRIPT_DIR}/Puppetfile \
-    --suffix '%d'
-  install_external
-  install_openstack
-else
-  install_all
-fi
+install_modules
 
 puppet module list
