ba8ee11132606c489c2df014cb106b9c43b7848c
Alex Schultz <aschultz@mirantis.com>
2015-10-15 09:59:57 -0500

    Fix scripts to be OSX compatible
    readlink on OSX does not support the -f option. As an alternative, we
    can use pwd -P to resolve the path.
    
    Change-Id: Ibaeadea9685f7a9603198ed1a509176cf70f9b21
    Closes-Bug: #1506504

--- install_modules_unit.sh
+++ install_modules_unit.sh
@@ -10,7 +10,7 @@ if [ ! -z ${GEM_HOME} ]; then
     GEM_BIN_DIR=${GEM_HOME}/bin/
 fi
 
-export SCRIPT_DIR=$(readlink -f "$(dirname $0)")
+export SCRIPT_DIR=$(cd `dirname $0` && pwd -P)
 export PUPPETFILE_DIR=${PUPPETFILE_DIR:-/etc/puppet/modules}
 source $SCRIPT_DIR/functions
 
