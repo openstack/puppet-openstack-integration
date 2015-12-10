2ee50978e70788e8db00370ce7144cebdddb24f8
Emilien Macchi <emilien@redhat.com>
2015-12-09 21:00:40 -0500

    Bump repos.pp to Mitaka on UCA & RDO
    Bump our CI to Mitaka packaging:
    * on Ubuntu using UCA proposed (testing) packages for Mitaka
    * on Centos using RDO current (master) packages for Mitaka
    
    Depends-On: I3a2e677c2bd349322cfb08d5dc73c1ced38c3f60
    Change-Id: I6f2d55f59ea57228f46e4733bfe952867f29dfa1

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -4,13 +4,29 @@ class openstack_integration::repos {
     'Debian': {
       include ::apt
       class { '::openstack_extras::repo::debian::ubuntu':
-        release         => 'liberty',
+        repo            => 'proposed',
+        release         => 'mitaka', # drop this line when mitaka is stable released
         package_require => true,
       }
     }
     'RedHat': {
       class { '::openstack_extras::repo::redhat::redhat':
-        release => 'liberty',
+        # pin a recent delorean but will be current-passed-ci later when RDO will be ready
+        manage_rdo => false,
+        repo_hash  => {
+          'mitaka-current' => {
+            'baseurl'  => 'http://trunk.rdoproject.org/centos7/f9/69/f9694ad5989d24363df812b08e36bd3b30807257_e3635ace/',
+            'descr'    => 'Mitaka trunk',
+            'gpgcheck' => 'no',
+            'priority' => 1,
+          },
+          'delorean-deps'  => {
+            'baseurl'  => 'http://buildlogs.centos.org/centos/7/cloud/$basearch/openstack-liberty/',
+            'descr'    => 'Liberty delorean-deps',
+            'gpgcheck' => 'no',
+            'priority' => 2,
+          },
+        }
       }
     }
     default: {
