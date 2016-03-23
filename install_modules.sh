cf864be1bc3ef8b0787d476865b2a52ebabeb6eb
Emilien Macchi <emilien@redhat.com>
2016-03-22 21:31:18 -0400

    install_modules: export PUPPET_BASE_PATH
    We need to export PUPPET_BASE_PATH otherwise beaker & unit CI jobs will
    fail to copy modules in the right path:
    mv /modules/openstack-integration /modules/openstack_integration
    
    Change-Id: I1c99de3f99367fb7677977fee4c5cf08afda5977

--- install_modules.sh
+++ install_modules.sh
@@ -6,6 +6,12 @@ if [ ! -z ${GEM_HOME} ]; then
     GEM_BIN_DIR=${GEM_HOME}/bin/
 fi
 
+if [ $PUPPET_VERSION == 4 ]; then
+  export PUPPET_BASE_PATH=/etc/puppetlabs/code
+else
+  export PUPPET_BASE_PATH=/etc/puppet
+fi
+
 export SCRIPT_DIR=$(cd `dirname $0` && pwd -P)
 export PUPPETFILE_DIR=${PUPPETFILE_DIR:-${PUPPET_BASE_PATH}/modules}
 source $SCRIPT_DIR/functions
