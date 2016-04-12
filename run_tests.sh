a3117a64499af9db8444cd1bc3862dcc50d96334
Emilien Macchi <emilien@redhat.com>
2016-04-12 09:21:55 -0400

    run_tests: install selinux tools
    SElinux tools will be useful for us to provide SElinux alerts reports
    during our CI jobs.
    
    Change-Id: Icce81078d91bbed12b9dbba3c53416476d594a09

--- run_tests.sh
+++ run_tests.sh
@@ -76,7 +76,8 @@ elif is_fedora; then
 
     wget  http://yum.puppetlabs.com/${PUPPET_RELEASE_FILE}-el-7.noarch.rpm -O /tmp/puppet.rpm
     $SUDO rpm -ivh /tmp/puppet.rpm
-    $SUDO yum install -y dstat ${PUPPET_PKG}
+    $SUDO yum install -y dstat ${PUPPET_PKG} setools setroubleshoot audit
+    $SUDO service auditd start
 fi
 
 PUPPET_ARGS="${PUPPET_ARGS} --detailed-exitcodes --verbose --color=false --debug"
