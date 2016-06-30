81887b553619d1660ecfe76f7033e0ad4600bbce
Emilien Macchi <emilien@redhat.com>
2016-06-30 10:18:17 -0400

    redhat/packages: set allow_virtual to True
    RDO packaging provides virtual packages. Puppet 3 is setting
    allow_virtual to False and in 4 it's set to True.
    Force it to True to avoid issues when deploying RDO.
    
    The cherry-pick to mitaka makes allow_virtual usage optional, only
    in the Puppet versions that support it (> 3.6). That should allow
    unit tests to pass in lower versions, while the integration tests
    can work with later releases.
    
    Change-Id: I54ade4cb4add005429840eb7d8970d736b1a0b22
    (cherry picked from commit 807a60e802c2de0c7ac9778447fc02c66fdc1683)

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,5 +9,14 @@ class openstack_integration {
     # temporary hack to make sure RabbitMQ does not steal UID
     # of Keystone
     Package<| title == 'keystone' |> -> Package<| title == 'rabbitmq-server' |>
+
+    # Some packages provided by RDO are virtual
+    # allow_virtual is false in Puppet 3 and will be true
+    # in Puppet 4. So let's set it to True.
+    # We still support Puppet 3 until distros ship Puppet 4 by default.
+    # Doing it conditionally as we still support Puppet < 3.6
+    if versioncmp($::puppetversion, '3.6.0') >= 0 {
+        Package<| tag == 'openstack' |> { allow_virtual => true }
+    }
   }
 }
