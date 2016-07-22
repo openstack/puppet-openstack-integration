6c7cb5a39d2b54a222605d9caf9973e8059e34e5
Denis Egorenko <degorenko@mirantis.com>
2016-07-22 11:32:30 +0300

    Use latest tempest for tests
    As Ubuntu updated repo to current Newton, we can use latest Tempest.
    
    Change-Id: If9275cd38fac8c4d6145fa985ee1dcc5953d0dea

--- run_tests.sh
+++ run_tests.sh
@@ -193,12 +193,6 @@ TESTS="${TESTS} TestManageQueue"
 print_header 'Running Tempest'
 cd /tmp/openstack/tempest
 
-if uses_debs; then
-    # TODO: we need to find why tempest tests fail when running
-    # latest Ubuntu Mitaka on Xenial and recent Tempest.
-    git checkout c9e37ae50f8f87bb39d64295bfd40c515b4edeba
-fi
-
 tox -eall-plugin -- --concurrency=2 $TESTS
 RESULT=$?
 set -e
