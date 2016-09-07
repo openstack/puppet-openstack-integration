509a71e493ceffc33452dcd5a255e444175eb519
Keith Schincke <keith.schincke@gmail.com>
2016-09-07 10:37:36 -0400

    Update to openstack_integration::ceph to use new ceph parameters
    This patch updates openstack_integration::ceph to use the
    osd_max_object_name_len and osd_max_object_namespace_len parameters.
    This will allow p-o-i jobs to use EXT4 file systems without
    adding parameters via ceph_extra_config
    
    Change-Id: Iea0dd5ae7bb1c438a68db0fd2af63bfa398efcd3
    Depends-On: Id487d6d656e88dfe29eac80c378058551c92dfd9

--- manifests/ceph.pp
+++ manifests/ceph.pp
@@ -9,16 +9,18 @@ class openstack_integration::ceph {
   }
 
   class { '::ceph::profile::params':
-    fsid                      => '7200aea0-2ddd-4a32-aa2a-d49f66ab554c',
-    manage_repo               => false, # repo already managed in openstack_integration::repo
-    ms_bind_ipv6              => $ms_bind_ipv6,
-    authentication_type       => 'cephx',
-    mon_host                  => $::openstack_integration::config::ip_for_url,
-    mon_initial_members       => $::hostname,
-    osd_pool_default_size     => '1',
-    osd_pool_default_min_size => '1',
-    mon_key                   => 'AQD7kyJQQGoOBhAAqrPAqSopSwPrrfMMomzVdw==',
-    client_keys               => {
+    fsid                         => '7200aea0-2ddd-4a32-aa2a-d49f66ab554c',
+    manage_repo                  => false, # repo already managed in openstack_integration::repo
+    ms_bind_ipv6                 => $ms_bind_ipv6,
+    authentication_type          => 'cephx',
+    mon_host                     => $::openstack_integration::config::ip_for_url,
+    mon_initial_members          => $::hostname,
+    osd_pool_default_size        => '1',
+    osd_pool_default_min_size    => '1',
+    mon_key                      => 'AQD7kyJQQGoOBhAAqrPAqSopSwPrrfMMomzVdw==',
+    osd_max_object_name_len      => 256,
+    osd_max_object_namespace_len => 64,
+    client_keys                  => {
       'client.admin'         => {
         'secret'  => 'AQD7kyJQQGoOBhAAqrPAqSopSwPrrfMMomzVdw==',
         'mode'    => '0600',
@@ -38,7 +40,7 @@ class openstack_integration::ceph {
         'cap_osd' => 'allow class-read object_prefix rbd_children, allow rwx pool=cinder, allow rwx pool=nova, allow rwx pool=glance, allow rwx pool=gnocchi',
       },
     },
-    osds                      => {
+    osds                         => {
       '/var/lib/ceph/data' => {},
     },
   }
@@ -53,8 +55,6 @@ class openstack_integration::ceph {
   $ceph_extra_config = {
     'client/rbd_default_features'         => { value => '15' },
     'global/osd_journal_size'             => { value => '100' },
-    'global/osd_max_object_namespace_len' => { value => '64' },
-    'global/osd_max_object_name_len'      => { value => '256' },
 
   }
   class { '::ceph::conf':
