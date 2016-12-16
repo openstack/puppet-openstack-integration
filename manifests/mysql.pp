7ced35129feb877768e74a52ceb565b2f1be8a8c
Alfredo Moralejo <amoralej@redhat.com>
2016-12-16 11:43:11 -0500

    Fix mariadb-libs issue for non p-o-i jobs
    CentOS 7.3 ships a new version of mariadb-libs which
    conflicts with the version in RDO.
    
    For p-o-i scenarios jobs this has been fixed in
    https://review.openstack.org/#/c/403164/ but rspec
    jobs which use openstack_integration::repos but not
    run_tests.sh are still failing.
    
    This should be reverted once a new non-conflicting package
    is released by RDO.
    
    Change-Id: Ib43ac25f96994c9373fd1e848f229ec713dd54b7
    (cherry picked from commit 3b6bc5bb3ee63cf23ae56cfbf7c102799f2480b4)

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -2,4 +2,13 @@ class openstack_integration::mysql {
 
   class { '::mysql::server': }
 
+  # FIXME (amoralej) Required until a new release of mariadb-libs is released by RDO
+  if $::osfamily == 'RedHat' {
+    package { 'mariadb-libs':
+      ensure => 'latest'
+    }
+    Package['mariadb-libs'] -> Class['mysql::server']
+    Package['mariadb-libs'] -> Class['mysql::client']
+  }
+
 }
