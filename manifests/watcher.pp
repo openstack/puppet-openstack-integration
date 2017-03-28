dd925bf167e9e928980a283c06f068278ef9c604
Emilien Macchi <emilien@redhat.com>
2017-03-28 15:38:23 +0000

    Revert "Fix watcher gnocchiclient requirement"
    We don't need this workaround anymore, we got a promotion with fixed package.
    
    This reverts commit 2348c0c847b13374f032ee71dd17e792c43f0265.
    
    Change-Id: I6f77b4f0978f80b05ad1b39257c490fa01fa7f05

--- manifests/watcher.pp
+++ manifests/watcher.pp
@@ -22,8 +22,6 @@ class openstack_integration::watcher {
       require => Package['watcher'],
     }
   }
-  # TODO(aschultz): remove this once the packaging has been fixed
-  include ::gnocchi::client
   class { '::watcher::db::mysql':
     password => 'watcher',
   }
