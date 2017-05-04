047dbb7280ac77449463620c13fbfd7dace7fc9b
Mehdi Abaakouk <sileht@sileht.net>
2017-05-04 09:39:10 +0200

    scenario001: rework telemetry services
    Ubuntu and Redhat/Centos doesn't have all telemetry packages.
    
    Instead of checking everywhere if we are on Ubuntu or Redhat/Centos,
    this change uses a boolean 'enable_telemetry_legacy'.
    
    If true, ::openstack_integration::ceilometer will configure ceilometer
    with legacy services/configuration (without panko/gnocchi). If false,
    Gnocchi and Panko backend is used.
    
    scenario001 set it to true for Ubuntu and false for Redhat/Centos.
    
    This centralize the kind of ceilometer deployement in one place.
    
    This change also drops the now useless collector configuration and
    configure the notification agent correctly instead.
    
    Change-Id: If229829975d4f25ca0a2a3c6c93e674204d71dc2

--- manifests/gnocchi.pp
+++ manifests/gnocchi.pp
@@ -3,71 +3,66 @@ class openstack_integration::gnocchi {
   include ::openstack_integration::config
   include ::openstack_integration::params
 
-  # gnocchi is not packaged in Ubuntu Cloud Archive
-  # https://bugs.launchpad.net/cloud-archive/+bug/1535740
-  if $::osfamily == 'RedHat' {
+  if $::openstack_integration::config::ssl {
+    openstack_integration::ssl_key { 'gnocchi':
+      notify  => Service['httpd'],
+      require => Package['gnocchi'],
+    }
+    Exec['update-ca-certificates'] ~> Service['httpd']
+  }
 
-    if $::openstack_integration::config::ssl {
-      openstack_integration::ssl_key { 'gnocchi':
-        notify  => Service['httpd'],
-        require => Package['gnocchi'],
-      }
-      Exec['update-ca-certificates'] ~> Service['httpd']
-    }
-
-    class { '::gnocchi':
-      debug               => true,
-      database_connection => 'mysql+pymysql://gnocchi:gnocchi@127.0.0.1/gnocchi?charset=utf8',
-    }
-    class { '::gnocchi::db::mysql':
-      password => 'gnocchi',
-    }
-    class { '::gnocchi::keystone::auth':
-      public_url   => "${::openstack_integration::config::base_url}:8041",
-      internal_url => "${::openstack_integration::config::base_url}:8041",
-      admin_url    => "${::openstack_integration::config::base_url}:8041",
-      password     => 'a_big_secret',
-    }
-    class { '::gnocchi::keystone::authtoken':
-      password            => 'a_big_secret',
-      user_domain_name    => 'Default',
-      project_domain_name => 'Default',
-      auth_url            => $::openstack_integration::config::keystone_admin_uri,
-      auth_uri            => $::openstack_integration::config::keystone_auth_uri,
-      memcached_servers   => $::openstack_integration::config::memcached_servers,
-    }
-    class { '::gnocchi::api':
-      enabled      => true,
-      service_name => 'httpd',
-      sync_db      => true,
-    }
-    include ::apache
-    class { '::gnocchi::wsgi::apache':
-      bind_host => $::openstack_integration::config::ip_for_url,
-      ssl       => $::openstack_integration::config::ssl,
-      ssl_key   => "/etc/gnocchi/ssl/private/${::fqdn}.pem",
-      ssl_cert  => $::openstack_integration::params::cert_path,
-      workers   => 2,
-    }
-    class { '::gnocchi::client': }
-    class { '::gnocchi::metricd': }
-    class { '::gnocchi::storage':
-      coordination_url => $::openstack_integration::config::tooz_url,
-    }
-    class { '::gnocchi::storage::ceph':
-      ceph_username => 'openstack',
-      ceph_keyring  => '/etc/ceph/ceph.client.openstack.keyring',
-    }
-    # make sure ceph pool exists before running gnocchi (dbsync & services)
-    Exec['create-gnocchi'] -> Exec['gnocchi-db-sync']
-    class { '::gnocchi::statsd':
-      archive_policy_name => 'high',
-      flush_delay         => '100',
-      # random datas:
-      resource_id         => '07f26121-5777-48ba-8a0b-d70468133dd9',
-      user_id             => 'f81e9b1f-9505-4298-bc33-43dfbd9a973b',
-      project_id          => '203ef419-e73f-4b8a-a73f-3d599a72b18d',
-    }
+  class { '::gnocchi':
+    debug               => true,
+    database_connection => 'mysql+pymysql://gnocchi:gnocchi@127.0.0.1/gnocchi?charset=utf8',
+  }
+  class { '::gnocchi::db::mysql':
+    password => 'gnocchi',
+  }
+  class { '::gnocchi::keystone::auth':
+    public_url   => "${::openstack_integration::config::base_url}:8041",
+    internal_url => "${::openstack_integration::config::base_url}:8041",
+    admin_url    => "${::openstack_integration::config::base_url}:8041",
+    password     => 'a_big_secret',
+  }
+  class { '::gnocchi::keystone::authtoken':
+    password            => 'a_big_secret',
+    user_domain_name    => 'Default',
+    project_domain_name => 'Default',
+    auth_url            => $::openstack_integration::config::keystone_admin_uri,
+    auth_uri            => $::openstack_integration::config::keystone_auth_uri,
+    memcached_servers   => $::openstack_integration::config::memcached_servers,
+  }
+  class { '::gnocchi::api':
+    enabled      => true,
+    service_name => 'httpd',
+    sync_db      => true,
+  }
+  include ::apache
+  class { '::gnocchi::wsgi::apache':
+    bind_host => $::openstack_integration::config::ip_for_url,
+    ssl       => $::openstack_integration::config::ssl,
+    ssl_key   => "/etc/gnocchi/ssl/private/${::fqdn}.pem",
+    ssl_cert  => $::openstack_integration::params::cert_path,
+    workers   => 2,
+  }
+  class { '::gnocchi::client': }
+  class { '::gnocchi::metricd': }
+  class { '::gnocchi::storage':
+    coordination_url => $::openstack_integration::config::tooz_url,
+  }
+  class { '::gnocchi::storage::ceph':
+    ceph_username => 'openstack',
+    ceph_keyring  => '/etc/ceph/ceph.client.openstack.keyring',
+  }
+  # make sure ceph pool exists before running gnocchi (dbsync & services)
+  Exec['create-gnocchi'] -> Exec['gnocchi-db-sync']
+  class { '::gnocchi::statsd':
+    archive_policy_name => 'high',
+    flush_delay         => '100',
+    # random datas:
+    resource_id         => '07f26121-5777-48ba-8a0b-d70468133dd9',
+    user_id             => 'f81e9b1f-9505-4298-bc33-43dfbd9a973b',
+    project_id          => '203ef419-e73f-4b8a-a73f-3d599a72b18d',
   }
 
 }
