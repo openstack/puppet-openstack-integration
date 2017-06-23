272e1034277c6bf90f4855a78072bd1ed2c6ae28
Alfredo Moralejo <amoralej@redhat.com>
2017-06-23 11:27:48 +0200

    Revert "Disable vitrage in scenario001"
    This reverts commit 81642e4f13f05de4f3e99aded3a5e049927f56c8.
    
    Change-Id: I0e05905f1842921df987f7f65b7863e005a97fb3
    Depends-On: I1d18f6c0d38bb66e03ab2977570310cb2357015c

--- manifests/vitrage.pp
+++ manifests/vitrage.pp
@@ -87,6 +87,7 @@ class openstack_integration::vitrage {
   }
   class { '::vitrage::graph': }
   class { '::vitrage::notifier': }
+  class { '::vitrage::collector': }
   class { '::vitrage::client': }
 
 }
