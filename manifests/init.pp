6ec2bf9602ad0142bebca7f1df38e1127e8414b2
Mohammed Naser <mnaser@vexxhost.com>
2017-09-13 16:04:30 +0000

    Revert "redhat/packages: set allow_virtual to True"
    We do not support Puppet 3 anymore, so this is not needed.
    
    This reverts commit 807a60e802c2de0c7ac9778447fc02c66fdc1683.
    
    Change-Id: I6f12ce48c18234855d907005dc04c4e4effd0187

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,5 @@ class openstack_integration {
     package { 'openstack-selinux':
         ensure => 'latest'
     }
-    # Some packages provided by RDO are virtual
-    # allow_virtual is false in Puppet 3 and will be true
-    # in Puppet 4. So let's set it to True.
-    # We still support Puppet 3 until distros ship Puppet 4 by default.
-    Package<| tag == 'openstack' |> { allow_virtual => true }
   }
 }
