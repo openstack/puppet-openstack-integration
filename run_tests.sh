f76ed87e6d9ac7d112eb78836c72b9f63cad665b
Chandan Kumar <chkumar@redhat.com>
2017-12-12 13:36:08 +0530

    Switch to stestr for generating subunit results
    * https://review.openstack.org/#/c/504345/ moves the tempest run
      cli to stestr. In order to avoid breakage switch to stestr from
      testr.
    * Check for .testresposity and .stestr folder and then use respective
      test runner. It will avoid the breakage in CI.
    
    Change-Id: Ic6c5fb4bdf883708cf4fc08486db7e89e0a76c6a
    (cherry picked from commit c5a7236f78dc9a5a81cf58d8f13041cd657b585e)
    
    Remove --regex from tempest command when running with RDO
    
    https://review.openstack.org/#/c/504345/ has changed the behavior of tempest
    when running with --regex and --whitelist-file [1]
    
    In order to promote we need to remove this regex option and implement [2] when ready
    after promotion.
    
    This patch is removing totally the --regex when not running in UCA. Note these tests
    were disabled in [3] and hopefully they are more stable now and we can run
    without skipping them until we can implement --blacklist-file.
    
    [1] https://bugs.launchpad.net/tempest/+bug/1751115
    [2] https://review.openstack.org/#/c/547278
    [3] https://review.openstack.org/#/c/461969/
    
    Change-Id: I0dd846a383ed2e1cb4c699a42e618156a9e628b2
    (cherry picked from commit 9325150ded93079a819a9800929a643402f3f532)

--- run_tests.sh
+++ run_tests.sh
@@ -250,7 +250,7 @@ $SUDO chown -R "$(id -u):$(id -g)" /tmp/openstack/tempest/
 # install from source now on ubuntu until packaged
 if uses_debs; then
     cd /tmp/openstack/tempest-horizon; $SUDO python setup.py install
-    $SUDO pip install -U testrepository os-testr
+    $SUDO pip install -U stestr os-testr
 fi
 
 set +e
@@ -308,7 +308,13 @@ if uses_debs; then
   # TODO (amoralej) tempest tests for object_storage are not working in master with current version of tempest in uca (16.1.0).
   EXCLUDES="--regex=^(?!mistral_tempest_tests.tests.api.v2.test_executions.ExecutionTestsV2.test_get_list_executions.*$)(?!ceilometer.tests.tempest.api.test_telemetry_notification_api.TelemetryNotificationAPITest.test_check_glance_v2_notifications.*$)(?!tempest.api.object_storage.*$).*"
 else
-  EXCLUDES="--regex=^(?!tempest.scenario.gnocchi.test.live_assert_vcpus_metric_is_really_expurged.test_request.*$)(?!tempest.scenario.gnocchi.test.live_assert_no_delete_metrics_have_the_gabbilive_policy.test_request.*$).*"
+  # https://review.openstack.org/#/c/504345/ has changed the behavior of tempest when running with --regex and --whitelist-file
+  # and now operator between them is OR when filtering tests (which is how it was documented, btw). In order to promote
+  # we need to remove this regex option and implement https://review.openstack.org/#/c/547278 when ready.
+  # Note these tests were disabled in https://review.openstack.org/#/c/461969/ and hopefully it's more stable now and allows
+  # us to run it until we can implement --blacklist-file in a stable way.
+  #EXCLUDES="--regex=^(?!tempest.scenario.gnocchi.test.live_assert_vcpus_metric_is_really_expurged.test_request.*$)(?!tempest.scenario.gnocchi.test.live_assert_no_delete_metrics_have_the_gabbilive_policy.test_request.*$).*"
+  EXCLUDES=""
 fi
 print_header 'Running Tempest'
 cd /tmp/openstack/tempest
@@ -327,7 +333,11 @@ $tempest_binary run --whitelist_file=/tmp/openstack/tempest/test-whitelist.txt -
 RESULT=$?
 set -e
 $tempest_binary list-plugins
-testr last --subunit > /tmp/openstack/tempest/testrepository.subunit
+if [ -d .testrepository ]; then
+    testr last --subunit > /tmp/openstack/tempest/testrepository.subunit
+elif [ -d .stestr ]; then
+    stestr last --subunit > /tmp/openstack/tempest/testrepository.subunit
+fi
 subunit2html /tmp/openstack/tempest/testrepository.subunit /tmp/openstack/tempest/testr_results.html
 print_header 'SELinux Alerts (Tempest)'
 catch_selinux_alerts
