d209db89f2450222e87e8aa4b08cea59865e26e1
Alfredo Moralejo <amoralej@redhat.com>
2018-02-07 14:15:27 +0100

    Remove trailing slash in NODEPOOL_PUPPETLABS_MIRROR
    Currently, variable NODEPOOL_PUPPETLABS_MIRROR for centos has an
    extra slash character at the end. It seems this is not a problem when
    using a mirror, but it fails when using puppetlabs yum repo as the
    url [1] gives an 404 error, while [2] works fine.
    
    [1] https://yum.puppetlabs.com//el/7/PC1/x86_64/repodata/repomd.xml
    [1] https://yum.puppetlabs.com/el/7/PC1/x86_64/repodata/repomd.xml
    
    Change-Id: Idf3a080322f117e62dec291f3047e9b5408813b9

--- configure_facts.sh
+++ configure_facts.sh
@@ -28,7 +28,7 @@ if [ -f /etc/ci/mirror_info.sh ]; then
         NODEPOOL_PUPPETLABS_MIRROR="http://${NODEPOOL_MIRROR_HOST}/apt-puppetlabs"
     else
         CEPH_MIRROR_HOST="${NODEPOOL_BUILDLOGS_CENTOS_PROXY}/centos/7/storage/x86_64/ceph-luminous/"
-        NODEPOOL_PUPPETLABS_MIRROR="http://${NODEPOOL_MIRROR_HOST}/yum-puppetlabs/"
+        NODEPOOL_PUPPETLABS_MIRROR="http://${NODEPOOL_MIRROR_HOST}/yum-puppetlabs"
     fi
 else
     CENTOS_MIRROR_HOST='http://mirror.centos.org'
@@ -40,7 +40,7 @@ else
         NODEPOOL_PUPPETLABS_MIRROR='https://apt.puppetlabs.com'
     else
         CEPH_MIRROR_HOST='https://buildlogs.centos.org/centos/7/storage/x86_64/ceph-luminous/'
-        NODEPOOL_PUPPETLABS_MIRROR="https://yum.puppetlabs.com/"
+        NODEPOOL_PUPPETLABS_MIRROR="https://yum.puppetlabs.com"
     fi
 fi
 
