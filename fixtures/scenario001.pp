00fe9dfe8f0bedd0aca8dc1cf91d0950d9306928
ZhongShengping <chdzsp@163.com>
2018-03-06 09:42:49 +0800

    Enable vitrage in scenario001
    Change-Id: I3caec2da4e154d5e62cf311c3a745e446ebdeb75

--- fixtures/scenario001.pp
+++ fixtures/scenario001.pp
@@ -26,7 +26,7 @@ case $::osfamily {
   }
   'RedHat': {
     $ipv6                    = true
-    $enable_vitrage          = false
+    $enable_vitrage          = true
     $om_rpc                  = 'amqp'
     $om_notify               = 'rabbit'
     $notification_topics     = ['notifications', 'vitrage_notifications']
