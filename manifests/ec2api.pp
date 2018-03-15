168629622fbe7a17da86c01a88ec60769c7b5a97
ZhongShengping <chdzsp@163.com>
2018-03-15 08:44:51 +0800

    Support Debian warning
    error[1] when testing on ubuntu system.
    
    [1]http://logs.openstack.org/51/552851/1/check/puppet-openstack-beaker-xenial/62d73e2/job-output.txt.gz#_2018-03-14_09_46_26_569462
    
    Change-Id: Iaf66cbc4bf1aebc98edf8df6cfb7f92d2d9e7e6f
    Closes-Bug: #1755953

--- manifests/ec2api.pp
+++ manifests/ec2api.pp
@@ -5,10 +5,6 @@ class openstack_integration::ec2api {
   include ::openstack_integration::config
   include ::openstack_integration::params
 
-  # Ec2api resources
-  class { '::ec2api::logging':
-    debug => true,
-  }
   class { '::ec2api::keystone::auth':
     public_url   => "${::openstack_integration::config::base_url}:8788",
     internal_url => "${::openstack_integration::config::base_url}:8788",
@@ -18,11 +14,14 @@ class openstack_integration::ec2api {
   class { '::ec2api::db::mysql':
     password => 'ec2api',
   }
-  class { '::ec2api::db::sync': }
   case $::osfamily {
     'RedHat': {
       class { '::ec2api::db':
         database_connection => 'mysql+pymysql://ec2api:ec2api@127.0.0.1/ec2api?charset=utf8',
+      }
+      class { '::ec2api::db::sync': }
+      class { '::ec2api::logging':
+        debug => true,
       }
       class { '::ec2api': }
       class { '::ec2api::keystone::authtoken':
@@ -35,6 +34,9 @@ class openstack_integration::ec2api {
         external_network        => 'public',
       }
       include ::ec2api::metadata
+    }
+    'Debian': {
+      warning('ec2api is not yet packaged on Ubuntu systems.')
     }
     default: {
       fail("Unsupported osfamily (${::osfamily})")
