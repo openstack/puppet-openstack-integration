74672785b13178d480bb8b28dea2f94a838d9847
zhubingbing <zhu.bingbing@99cloud.net>
2018-04-24 20:12:02 +0800

    Switch to www_authenticate_uri for aodh
    puppet-aodh switched to www_authenticate_uri in
    [1], update puppet-openstack-integration for the same.
    
    [1] https://review.openstack.org/#/c/558340/
    
    Change-Id: I7ceb1eb1f976d877022b29125acc47b609dd4f43

--- manifests/aodh.pp
+++ manifests/aodh.pp
@@ -64,12 +64,12 @@ class openstack_integration::aodh (
     password     => 'a_big_secret',
   }
   class { '::aodh::keystone::authtoken':
-    password            => 'a_big_secret',
-    user_domain_name    => 'Default',
-    project_domain_name => 'Default',
-    auth_url            => $::openstack_integration::config::keystone_admin_uri,
-    auth_uri            => $::openstack_integration::config::keystone_auth_uri,
-    memcached_servers   => $::openstack_integration::config::memcached_servers,
+    password             => 'a_big_secret',
+    user_domain_name     => 'Default',
+    project_domain_name  => 'Default',
+    auth_url             => $::openstack_integration::config::keystone_admin_uri,
+    www_authenticate_uri => $::openstack_integration::config::keystone_auth_uri,
+    memcached_servers    => $::openstack_integration::config::memcached_servers,
   }
   class { '::aodh::api':
     enabled      => true,
