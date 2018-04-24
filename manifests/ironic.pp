a5ed4a0384468f723d398fc3dc6adb8b66a4768c
zhubingbing <zhu.bingbing@99cloud.net>
2018-04-24 20:31:17 +0800

    Switch to www_authenticate_uri for ironic
    puppet-ironic switched to www_authenticate_uri in
    [1], update puppet-openstack-integration for the same.
    
    [1] https://review.openstack.org/#/c/558450/
    
    Change-Id: Ifeed69d16f509c5e5b6668056e0b816ec9d73268

--- manifests/ironic.pp
+++ manifests/ironic.pp
@@ -39,12 +39,12 @@ class openstack_integration::ironic {
     password     => 'a_big_secret',
   }
   class { '::ironic::api::authtoken':
-    password            => 'a_big_secret',
-    user_domain_name    => 'Default',
-    project_domain_name => 'Default',
-    auth_url            => $::openstack_integration::config::keystone_admin_uri,
-    auth_uri            => $::openstack_integration::config::keystone_auth_uri,
-    memcached_servers   => $::openstack_integration::config::memcached_servers,
+    password             => 'a_big_secret',
+    user_domain_name     => 'Default',
+    project_domain_name  => 'Default',
+    auth_url             => $::openstack_integration::config::keystone_admin_uri,
+    www_authenticate_uri => $::openstack_integration::config::keystone_auth_uri,
+    memcached_servers    => $::openstack_integration::config::memcached_servers,
   }
   class { '::ironic::keystone::auth_inspector':
     password => 'a_big_secret',
