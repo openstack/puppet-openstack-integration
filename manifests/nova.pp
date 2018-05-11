7ed956b5f0e31470a1297369f89ee0ef5313d846
zhubingbing <zhu.bingbing@99cloud.net>
2018-05-11 14:32:51 +0800

    Switch to www_authenticate_uri for nova
    puppet-nova switched to www_authenticate_uri in
    [1], update puppet-openstack-integration for the same.
    
    [1] https://review.openstack.org/#/c/558451/
    
    Change-Id: I79af66fdbbf2f81a3da4900053d0f2e92b0fcaac

--- manifests/nova.pp
+++ manifests/nova.pp
@@ -105,12 +105,12 @@ class openstack_integration::nova (
     }
   }
   class { '::nova::keystone::authtoken':
-    password            => 'a_big_secret',
-    user_domain_name    => 'Default',
-    project_domain_name => 'Default',
-    auth_url            => $::openstack_integration::config::keystone_admin_uri,
-    auth_uri            => $::openstack_integration::config::keystone_auth_uri,
-    memcached_servers   => $::openstack_integration::config::memcached_servers,
+    password             => 'a_big_secret',
+    user_domain_name     => 'Default',
+    project_domain_name  => 'Default',
+    auth_url             => $::openstack_integration::config::keystone_admin_uri,
+    www_authenticate_uri => $::openstack_integration::config::keystone_auth_uri,
+    memcached_servers    => $::openstack_integration::config::memcached_servers,
   }
   class { '::nova':
     default_transport_url         => $default_transport_url,
