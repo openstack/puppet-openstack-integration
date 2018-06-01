46b503f7b02f9aabad953776c2d8b4eb02859dda
Alex Schultz <aschultz@redhat.com>
2018-06-01 08:08:30 -0600

    Make fix redis service setup Debian only
    This is an improvemen tonI1b37524ff1071d87e8a7df2fa3bca66aed162d1b
    so that it only disables the service management when debian is used.
    
    Change-Id: Ib69317e48f4c8b9e2eb69bcb31c8245db7b286ed

--- manifests/redis.pp
+++ manifests/redis.pp
@@ -1,8 +1,17 @@
 class openstack_integration::redis {
   include ::openstack_integration::config
 
+  # due to issues in OpenStack CI with the redis package, we need to disable
+  # the service enable flag. The service still starts but the management of
+  # the service with systemd errors.
+  if ($::os_package_type == 'debian') {
+    $service_enable = false
+  } else {
+    $service_enable = true
+  }
+
   class { '::redis':
     bind           => $::openstack_integration::config::host,
-    service_enable => false
+    service_enable => $service_enable
   }
 }
