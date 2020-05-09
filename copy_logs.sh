f4a8ec0fde3e6da873258bbc0bc75b5aa6566b3f
Takashi Kajinami <tkajinam@redhat.com>
2020-05-10 02:07:08 +0900

    Refactor installation of puppet packages
    Conflicts:
    	playbooks/prepare-node-common.yaml
    
    Resolved conflict caused by [1]
    
    [1] https://review.opendev.org/#/c/736069/
    
    Change-Id: I74e3b26dfbd5131eb219810a1418e085a7d41a81
    (cherry picked from commit 3a3aa790ef7c5aea5d4d661174dc1f2af20229fb)

--- copy_logs.sh
+++ copy_logs.sh
@@ -205,7 +205,7 @@ if [ `command -v dpkg` ]; then
 fi
 if [ `command -v rpm` ]; then
     rpm -qa |sort > $LOG_DIR/rpm-qa.txt
-    $YUM repolist -v > $LOG_DIR/repolist.txt
+    sudo $YUM repolist -v > $LOG_DIR/repolist.txt
 fi
 
 if [ `command -v gem` ]; then
