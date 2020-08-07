63f6ba4d15a4ed2f8c81a32fef0f6929ca85d381
Takashi Kajinami <tkajinam@redhat.com>
2020-08-07 09:05:11 +0900

    Implement a common class to manage apache service
    This patch introduces a new class, openstack_integration::apache, which
    can be used to manage apache service in ci jobs.
    
    Change-Id: I1d29662abe630f291fc4dbcb80d439d9e7388ce8
    (cherry picked from commit fc3f5bfbd38612bb74e1671db383a717b878deb2)

--- fixtures/scenario003.pp
+++ fixtures/scenario003.pp
@@ -14,28 +14,6 @@
 # limitations under the License.
 #
 
-if ($::os['family'] == 'Debian') {
-  $wsgi_mod_package = 'libapache2-mod-wsgi-py3'
-  $wsgi_mod_lib     = 'mod_wsgi.so'
-}
-elsif ($::os['name'] == 'Fedora') or
-  ($::os['family'] == 'RedHat' and Integer.new($::os['release']['major']) > 7) {
-  $wsgi_mod_package = 'python3-mod_wsgi'
-  $wsgi_mod_lib     = 'mod_wsgi_python3.so'
-}
-if ($::os['family'] == 'Debian') or ($::os['name'] == 'Fedora') or
-  ($::os['family'] == 'RedHat' and Integer.new($::os['release']['major']) > 7) {
-  include apache::params
-  class { 'apache':
-    mod_packages => merge($::apache::params::mod_packages, {
-      'wsgi' => $wsgi_mod_package,
-    }),
-    mod_libs     => merge($::apache::params::mod_libs, {
-      'wsgi' => $wsgi_mod_lib,
-    })
-  }
-}
-
 if ($::os['name'] == 'Ubuntu') or ($::os['name'] == 'Fedora') or
   ($::os['family'] == 'RedHat' and Integer.new($::os['release']['major']) > 7) {
   # FIXME(ykarel) Disable SSL until services are ready to work with SSL + Python3
@@ -92,6 +70,7 @@ class { 'openstack_integration::config':
 if $ssl {
   include openstack_integration::cacert
 }
+include openstack_integration::apache
 include openstack_integration::memcached
 include openstack_integration::rabbitmq
 include openstack_integration::mysql
