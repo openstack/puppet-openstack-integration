9a9066dc6b321f2fb29866c2591e350a4396e347
Takashi Kajinami <tkajinam@redhat.com>
2020-10-08 22:57:49 +0900

    Use Ussuri for Ubuntu
    This patch makes ubuntu tests use Ussuri since it is the latest
    version available now. The version will be bumped to victoria once
    Victoria release is available in Cloud Archive.
    
    Change-Id: I5c196885d4d83b8a29748cc00859915de8b4243f
    (cherry picked from commit 594cab64cd4a5ba565aa2304d3360e4ead70908c)
    (cherry picked from commit ea5ca8a7e79899eebd4567fc2bda34bf56867de4)

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -12,7 +12,7 @@ class openstack_integration::repos {
         'ubuntu': {
           include apt
           class { 'openstack_extras::repo::debian::ubuntu':
-            release         => 'train',
+            release         => 'ussuri',
             package_require => true,
             uca_location    => pick($::uca_mirror_host, 'http://ubuntu-cloud.archive.canonical.com/ubuntu'),
           }
