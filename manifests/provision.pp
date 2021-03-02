e1740e1a217ab76effab3625f18eeeaac5b85a68
Alfredo Moralejo <amoralej@redhat.com>
2021-03-02 11:53:20 +0100

    Update the cirros version to 0.5.1
    We are hitting random timeout issues when intances trying to get
    metadata. This version includes a fix for this [1].
    
    [1] https://github.com/cirros-dev/cirros/pull/11/commits/e40bcd2964aa496a9d03e1aaf95cf7a86938f129
    
    Change-Id: I7ee221be2a01b0e8b22b51902a81fe29bb624db3
    (cherry picked from commit 90671b2e0b05ea908a375c5bdb271afb55014870)

--- manifests/provision.pp
+++ manifests/provision.pp
@@ -84,14 +84,14 @@ class openstack_integration::provision (
       container_format => 'bare',
       disk_format      => 'qcow2',
       is_public        => 'yes',
-      source           => '/tmp/openstack/image/cirros-0.4.0-x86_64-disk.img'
+      source           => '/tmp/openstack/image/cirros-0.5.1-x86_64-disk.img'
     }
     glance_image { 'cirros_alt':
       ensure           => present,
       container_format => 'bare',
       disk_format      => 'qcow2',
       is_public        => 'yes',
-      source           => '/tmp/openstack/image/cirros-0.4.0-x86_64-disk.img'
+      source           => '/tmp/openstack/image/cirros-0.5.1-x86_64-disk.img'
     }
     Keystone_user_role['admin@openstack'] -> Glance_image<||>
   }
