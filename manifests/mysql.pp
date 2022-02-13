fc220e0a0824b62f74d74db047119bc5d4c48061
Takashi Kajinami <tkajinam@redhat.com>
2022-02-13 12:50:41 +0900

    MySQL: Remove workaround for old mariadb-libs in CentOS7
    This reverts the commit 3b6bc5bb3ee63cf23ae56cfbf7c102799f2480b4 , and
    removes the workaround added by that change, because
    
    - We no longer use CentOS 7 in CI
    
    - The issue is supposed to have been fixed in a recent mariadb-libs
      package.
    
    Change-Id: I05881459950c716430190ceba0813ac15f92da85

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -2,14 +2,4 @@ class openstack_integration::mysql {
 
   class { 'mysql::server': }
 
-  # FIXME (amoralej) Required until a new release of mariadb-libs is released by RDO
-  # Fedora and CentOS8 do not have mariadb-libs, so changing only for CentOS
-  if ($::operatingsystem == 'CentOS') and (versioncmp($::operatingsystemmajrelease, '7') == 0) {
-    package { 'mariadb-libs':
-      ensure => 'latest'
-    }
-    Package['mariadb-libs'] -> Class['mysql::server']
-    Package['mariadb-libs'] -> Class['mysql::client']
-  }
-
 }
