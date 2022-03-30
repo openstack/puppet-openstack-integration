28bc055fb92adfb1a4d801d97b99b583f61298d3
Takashi Kajinami <tkajinam@redhat.com>
2022-03-30 22:49:08 +0900

    Integration jobs: Increase default swap size
    This change increases default swap size, installed by the run-test.sh
    script, from 4GiB to 8GiB, because now we see frequent failures caused
    by memory shortage.
    
    This removes the separate ansible task to create an additional swap
    space (added by f4429e07d8b5afd9ff46ca907d6b7c18b9e7f93d ) because now
    the enough amount is created by run-test.sh.
    
    Change-Id: I24ebef234c79e636b24e47535732aea74e52a241

--- run_tests.sh
+++ run_tests.sh
@@ -23,7 +23,7 @@ export SCENARIO=${SCENARIO:-scenario001}
 export MANAGE_PUPPET_MODULES=${MANAGE_PUPPET_MODULES:-true}
 export MANAGE_REPOS=${MANAGE_REPOS:-true}
 export ADD_SWAP=${ADD_SWAP:-true}
-export SWAP_SIZE_GB=${SWAP_SIZE_GB:-4}
+export SWAP_SIZE_GB=${SWAP_SIZE_GB:-8}
 export HIERA_CONFIG=${HIERA_CONFIG:-${SCRIPT_DIR}/hiera.yaml}
 export MANAGE_HIERA=${MANAGE_HIERA:-true}
 export PUPPET_ARGS="${PUPPET_ARGS} --detailed-exitcodes --color=false --test --summarize --trace --hiera_config ${HIERA_CONFIG} --logdest ${WORKSPACE}/puppet.log"
