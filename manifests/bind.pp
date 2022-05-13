0946b53eed9cbcf18f8188df832bb12c1ff46d5d
Takashi Kajinami <tkajinam@redhat.com>
2022-05-13 11:38:59 +0900

    Remove redundant override of dns::controls
    The value is exactly same as the default value[1].
    
    [1] https://github.com/theforeman/puppet-dns/blob/9.3.0/manifests/params.pp#L116-L122
    
    Change-Id: Icee4d6a7fb670796de999e55502fe8226eb3a183

--- manifests/bind.pp
+++ manifests/bind.pp
@@ -6,21 +6,11 @@ class openstack_integration::bind {
   include openstack_integration::config
   include openstack_integration::params
 
-  # Need some particular options that aren't default in ::dns when using
-  # designate with bind9 backend. Set them up.
-  $controls = {
-    '127.0.0.1' => {
-      'port'              => 953,
-      'allowed_addresses' => [ '127.0.0.1' ],
-      'keys'              => [ 'rndc-key' ]
-    },
-  }
   # NOTE (dmsimard): listen_on_v6 is false and overridden due to extended port
   # configuration in additional_options
   class { 'dns':
     recursion          => 'no',
     allow_recursion    => [],
-    controls           => $controls,
     listen_on_v6       => false,
     additional_options => {
       'listen-on'     => 'port 5322 { any; }',
