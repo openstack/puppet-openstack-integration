openstack/puppet-openstack-integration

commit e7ffabe5bbf60ea510902b53a4a5edfb23bc4d2a

Use clouds.yaml to get keystone resources
... to use the system-admin user who has access to these API even when
SRBAC is enforced in Keystone.

Depends-on: https://review.opendev.org/806474
Change-Id: If796fd83c4d1ba47b290997515c2eab74ce1c29a

copy_logs.sh
@@ -272,6 +272,8 @@ fi
 # system status & informations
 sudo cp /root/openrc $LOG_DIR/openrc.txt
 sudo chmod 777 $LOG_DIR/openrc.txt
+sudo cp -r /etc/openstack $LOG_DIR/etc
+sudo chmod 777 $LOG_DIR/etc/admin-clouds.yaml
 df -h > $LOG_DIR/df.txt
 free -m > $LOG_DIR/free.txt
 lsmod > $LOG_DIR/lsmod.txt
@@ -294,9 +296,10 @@ done > $LOG_DIR/iptables.txt
 
 mkdir -p $LOG_DIR/openstack_resources
 
+export OS_CLOUD=project
+export OS_CLIENT_CONFIG_FILE=$LOG_DIR/etc/admin-clouds.yaml
 # keystone resources
 if [ -d $LOG_DIR/keystone ]; then
-    source $LOG_DIR/openrc.txt
     openstack >> $LOG_DIR/openstack_resources/keystone.txt <<-EOC
 extension list --identity
 endpoint list
@@ -310,7 +313,6 @@ fi
 
 # nova resources
 if [ -d $LOG_DIR/nova ]; then
-    source $LOG_DIR/openrc.txt
     openstack >> $LOG_DIR/openstack_resources/nova.txt <<-EOC
 extension list --compute
 compute service list
@@ -321,7 +323,6 @@ fi
 
 # cinder resources
 if [ -d $LOG_DIR/cinder ]; then
-    source $LOG_DIR/openrc.txt
     openstack >> $LOG_DIR/openstack_resources/cinder.txt <<-EOC
 extension list --volume
 volume service list
@@ -332,7 +333,6 @@ fi
 
 # glance resources
 if [ -d $LOG_DIR/glance ]; then
-    source $LOG_DIR/openrc.txt
     openstack >> $LOG_DIR/openstack_resources/glance.txt <<-EOC
 image list --long
 EOC
@@ -340,7 +340,6 @@ fi
 
 # neutron resources
 if [ -d $LOG_DIR/neutron ]; then
-    source $LOG_DIR/openrc.txt
     openstack >> $LOG_DIR/openstack_resources/neutron.txt <<-EOC
 extension list --network
 network agent list
@@ -353,6 +352,9 @@ network show public
 subnet show public-subnet
 EOC
 fi
+
+unset OS_CLOUD
+unset OS_CLIENT_CONFIG_FILE
 
 # end of log capture
 set -e

commit 093db0671485b3b4ae59af9646a27db406b5c6b9

Fix wrong path for clouds.yaml
This fixes the wrong path used to access clouds.yaml, which was
introduced by e7ffabe5bbf60ea510902b53a4a5edfb23bc4d2a .

Change-Id: I67347e0e548d9c24d2858ffd1ed0efa69b14055a

copy_logs.sh
@@ -274,7 +274,7 @@ fi
 sudo cp /root/openrc $LOG_DIR/openrc.txt
 sudo chmod 777 $LOG_DIR/openrc.txt
 sudo cp -r /etc/openstack $LOG_DIR/etc
-sudo chmod 777 $LOG_DIR/etc/admin-clouds.yaml
+sudo chmod 777 $LOG_DIR/etc/openstack/puppet/admin-clouds.yaml
 df -h > $LOG_DIR/df.txt
 free -m > $LOG_DIR/free.txt
 lsmod > $LOG_DIR/lsmod.txt
@@ -298,7 +298,7 @@ done > $LOG_DIR/iptables.txt
 mkdir -p $LOG_DIR/openstack_resources
 
 export OS_CLOUD=project
-export OS_CLIENT_CONFIG_FILE=$LOG_DIR/etc/admin-clouds.yaml
+export OS_CLIENT_CONFIG_FILE=$LOG_DIR/etc/openstack/puppet/admin-clouds.yaml
 # keystone resources
 if [ -d $LOG_DIR/keystone ]; then
     openstack >> $LOG_DIR/openstack_resources/keystone.txt <<-EOC