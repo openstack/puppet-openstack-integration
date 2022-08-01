8bf381e97bc3d2cde19f0bf992cce9091691c71e
Takashi Kajinami <tkajinam@redhat.com>
2022-08-02 08:17:09 +0900

    Ubuntu: Use utf8mb3_general_ci collate in MySQL
    Currently idempotency in Ubuntu is broken because of the below change
    detected in collate in MySQL.
    
    ```
    /Stage[main]/Keystone::Db::Mysql/Openstacklib::Db::Mysql[keystone]/
    Mysql_database[keystone]/collate: collate changed 'utf8mb3_general_ci'
    to 'utf8_general_ci'
    ```
    
    Similarly to what we observed in the past about charset[1], it seems
    MySQL in Ubuntu is automatically converting the collate value and that
    is causing the "unexpected" change detected in the 2nd puppet run.
    
    This fixes the idempotency by using utf8mb3_general_ci in Ubuntu to
    avoid the mismatch caused by internal translation.
    
    [1] 085d3569021441810c1dff7c8f4396003c690805
    
    Conflicts:
    	manifests/ironic.pp
    
    (yoga to xena)
    Conflicts:
    	manifests/octavia.pp
    
    Change-Id: I56f31397669d3d7b08aa2e9808947141e003ab0b
    (cherry picked from commit 9c2d58221c6f549aaca66643998ca06eab9c0641)
    (cherry picked from commit b8ad2df96a063145eeea5a8be1598e192637750f)

--- manifests/barbican.pp
+++ manifests/barbican.pp
@@ -19,6 +19,7 @@ class openstack_integration::barbican {
   include barbican
   class { 'barbican::db::mysql':
     charset  => $::openstack_integration::params::mysql_charset,
+    collate  => $::openstack_integration::params::mysql_collate,
     password => 'barbican',
   }
   class { 'barbican::db':
--- manifests/glance.pp
+++ manifests/glance.pp
@@ -29,6 +29,7 @@ class openstack_integration::glance (
 
   class { 'glance::db::mysql':
     charset  => $::openstack_integration::params::mysql_charset,
+    collate  => $::openstack_integration::params::mysql_collate,
     password => 'glance',
   }
   include glance
