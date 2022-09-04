0a562313dfeeb7492b91d97d315ec0588f61fa10
Takashi Kajinami <tkajinam@redhat.com>
2022-09-04 20:39:16 +0900

    Fix outdated test case name for volume backup tests
    Cinder V2 API tests were renamed long ago[1] and
    the VolumesBackupsAdminV2Test no longer exists.
    
    [1] e8afb8c2c5f948ee45e1c7edab4d82516920bd9a
    
    Conflicts:
    	run_tests.sh
    
    Resolved conflict caused by dab263aae2f473e1587f8b6d8acd7fcc8755b67c .
    
    Change-Id: I7ada16c0db4246facc3a1c4745cef4763479414b
    (cherry picked from commit d8e972207482dd5407eb1ea8af6cab53ddca6515)
    (cherry picked from commit e71d2c059b22bcd2515ba9f5d12a9b611e550161)
    (cherry picked from commit 0eaab598a2b365b0860013678fc7ac66027ef8e7)
    (cherry picked from commit bef013039c70d10d10e27ecef8f56aa599b16b75)

--- run_tests.sh
+++ run_tests.sh
@@ -308,7 +308,7 @@ echo "v2.test_queues.TestManageQueue" >> /tmp/openstack/tempest/test-whitelist.t
 echo "ec2api_tempest_plugin.api.*test_create_delete(?!.*_vpn_connection)" >> /tmp/openstack/tempest/test-whitelist.txt
 
 # Cinder Backup
-echo "VolumesBackupsAdminV2Test" >> /tmp/openstack/tempest/test-whitelist.txt
+echo "VolumesBackupsAdminTest" >> /tmp/openstack/tempest/test-whitelist.txt
 
 # Cinder encrypted volumes
 echo "TestEncryptedCinderVolumes" >> /tmp/openstack/tempest/test-whitelist.txt
