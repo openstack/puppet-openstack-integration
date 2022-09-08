openstack/puppet-openstack-integration

commit 25ad653e67f2c1e1c40868cb9b4ae13fe070c717

Test deployment of Octavia dashboard
This change enables horizon and its octavia dashboard plugin in
scenario 004 to test that deployment pattern.

Change-Id: Ie54427cf00292cb7225cdfa7a62a3b3e0eb2b7fb

fixtures/scenario004.pp
@@ -69,6 +69,9 @@ class { 'openstack_integration::ceph':
   deploy_rgw   => true,
   swift_dropin => true,
 }
+class { 'openstack_integration::horizon':
+  octavia_enabled => true
+}
 if $watcher_enabled {
   include openstack_integration::watcher
 }
@@ -79,6 +82,7 @@ include openstack_integration::provision
 # Don't test swift, radosgw won't pass the current tests
 # Glance, nova, neutron are true by default.
 class { 'openstack_integration::tempest':
+  horizon     => true,
   watcher     => $watcher_enabled,
   bgpvpn      => $bgpvpn_enabled,
   l2gw        => $l2gw_enabled,

manifests/horizon.pp
@@ -4,8 +4,13 @@
 #  (optional) Flag to enable heat dashboard
 #  Defaults to false.
 #
+# [*octavia_enabled*]
+#  (optional) Flag to enable octavia dashboard
+#  Defaults to false.
+#
 class openstack_integration::horizon (
-  $heat_enabled = false,
+  $heat_enabled    = false,
+  $octavia_enabled = false,
 ) {
 
   include openstack_integration::config
@@ -56,5 +61,8 @@ class openstack_integration::horizon (
 
   if $heat_enabled {
     class { 'horizon::dashboards::heat': }
+  }
+  if $octavia_enabled {
+    class { 'horizon::dashboards::octavia': }
   }
 }

commit bbc3a47da3493fffac88300f5d1868271e2a8b2d

Ubuntu: Enable watcher service
The watcher packages are already available in Ubuntu. This enables
the service in Ubuntu integration jobs to test Watcher deployment in
Ubuntu as well as CentOS.

Conflicts:
	fixtures/scenario004.pp

Resolved conflict caused by 25ad653e67f2c1e1c40868cb9b4ae13fe070c717 .

Change-Id: Iece72956f34e930ca4cd46bcbdb209199671607a
(cherry picked from commit 4b26004426934e1cd8ddb3a6ce1c2c6c43362be7)

fixtures/scenario004.pp
@@ -22,8 +22,6 @@ if $::os['name'] == 'Ubuntu' {
 
 if $::operatingsystem == 'Ubuntu' {
   $ipv6                = false
-  # Watcher packages are not available in Ubuntu repository.
-  $watcher_enabled     = false
   # TODO(tobias-urdin): No service plugin 'BGPVPN'
   $bgpvpn_enabled      = false
   # TODO(tobias-urdin): Plugin 'networking_l2gw.services.l2gateway.plugin.L2GatewayPlugin' not found.
@@ -32,7 +30,6 @@ if $::operatingsystem == 'Ubuntu' {
   $bgp_dragent_enabled = false
 } else {
   $ipv6                = true
-  $watcher_enabled     = true
   $bgpvpn_enabled      = true
   $l2gw_enabled        = true
   $bgp_dragent_enabled = true
@@ -69,9 +66,7 @@ class { 'openstack_integration::ceph':
   deploy_rgw   => true,
   swift_dropin => true,
 }
-if $watcher_enabled {
-  include openstack_integration::watcher
-}
+include openstack_integration::watcher
 include openstack_integration::octavia
 
 include openstack_integration::provision
@@ -79,7 +74,7 @@ include openstack_integration::provision
 # Don't test swift, radosgw won't pass the current tests
 # Glance, nova, neutron are true by default.
 class { 'openstack_integration::tempest':
-  watcher     => $watcher_enabled,
+  watcher     => true,
   bgpvpn      => $bgpvpn_enabled,
   l2gw        => $l2gw_enabled,
   l2gw_switch => 'cell08-5930-01::FortyGigE1/0/1|100',