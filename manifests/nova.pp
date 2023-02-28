openstack/puppet-openstack-integration

commit 3bb65c97a4c400bf82a836938f63b1b99f1c7757

Nova: Use appropriate IP version for novnproxy
... instead of using IP v6 regardless of the version tested in that
scenario.

Change-Id: Ia3fa98f4c2d944545b26dc3c8492d3eabd28f1aa

manifests/nova.pp
@@ -214,7 +214,9 @@ class openstack_integration::nova (
   }
   class { 'nova::scheduler': }
   class { 'nova::scheduler::filter': }
-  class { 'nova::vncproxy': }
+  class { 'nova::vncproxy':
+    host => $::openstack_integration::confing::host
+  }
 
   class { 'nova::network::neutron':
     auth_url              => "${::openstack_integration::config::keystone_admin_uri}/v3",

commit 575006d8849ffe5ed3391a4b79f0258c47359771

Fix typo in parameter name
Commit 3bb65c97a4c400bf82a836938f63b1b99f1c7757 introduced a type and
causing the following warning.

Unknown variable: '::openstack_integration::confing::host'.

This change fixes it.

Change-Id: Ied8eb6aeaa3385cc2f78ea9bd826bcbd68f4d826

manifests/nova.pp
@@ -231,7 +231,7 @@ class openstack_integration::nova (
   class { 'nova::scheduler': }
   class { 'nova::scheduler::filter': }
   class { 'nova::vncproxy':
-    host => $::openstack_integration::confing::host
+    host => $::openstack_integration::config::host
   }
 
   class { 'nova::network::neutron':