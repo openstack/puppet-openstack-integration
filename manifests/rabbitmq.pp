fe83b5488d33b75be6a9bba97faa8ddd69210973
Takashi Kajinami <tkajinam@redhat.com>
2023-03-07 23:29:47 +0900

    Refactor usage of puppet-rabbitmq
    This refactors current usage of puppet-rabbitmq. Details follow below.
    
    - the rabbitmql class supports the ipv6 parameter which injects
      the required definitions to use IPv6
    
    - ssl options are ignored if rabbitlqm::ssl is false
    
    - we can use native parameters to define the bind address
    
    Change-Id: Id33b56f84b04f0086f0e19e1938f36d109e4a188

--- manifests/rabbitmq.pp
+++ manifests/rabbitmq.pp
@@ -16,26 +16,29 @@ class openstack_integration::rabbitmq {
       require  => File['/etc/rabbitmq/ssl/private'],
       notify   => Service['rabbitmq-server'],
     }
-    class { 'rabbitmq':
-      package_provider      => $facts['package_provider'],
-      delete_guest_user     => true,
-      ssl                   => true,
-      ssl_only              => true,
-      ssl_cacert            => $::openstack_integration::params::ca_bundle_cert_path,
-      ssl_cert              => $::openstack_integration::params::cert_path,
-      ssl_key               => "/etc/rabbitmq/ssl/private/${facts['networking']['fqdn']}.pem",
-      environment_variables => $::openstack_integration::config::rabbit_env,
-      repos_ensure          => false,
-      manage_python         => false,
-    }
-  } else {
-    class { 'rabbitmq':
-      package_provider      => $facts['package_provider'],
-      delete_guest_user     => true,
-      environment_variables => $::openstack_integration::config::rabbit_env,
-      repos_ensure          => false,
-      manage_python         => false,
-    }
+  }
+
+  class { 'rabbitmq':
+    package_provider      => $facts['package_provider'],
+    delete_guest_user     => true,
+    ssl                   => $::openstack_integration::config::ssl,
+    ssl_only              => $::openstack_integration::config::ssl,
+    # the parameters below are ignored when ssl is false
+    ssl_cacert            => $::openstack_integration::params::ca_bundle_cert_path,
+    ssl_cert              => $::openstack_integration::params::cert_path,
+    ssl_key               => "/etc/rabbitmq/ssl/private/${facts['networking']['fqdn']}.pem",
+    environment_variables => {
+      'LC_ALL' => 'en_US.UTF-8',
+    },
+    repos_ensure          => false,
+    manage_python         => false,
+    # the interface parameter is ignored if ssl_only is true
+    interface             => $::openstack_integration::config::host,
+    # the ssl_interface parameter is ignored if ssl is false
+    ssl_interface         => $::openstack_integration::config::host,
+    node_ip_address       => $::openstack_integration::config::host,
+    management_ip_address => $::openstack_integration::config::host,
+    ipv6                  => $::openstack_integration::config::ipv6,
   }
   rabbitmq_vhost { '/':
     provider => 'rabbitmqctl',
