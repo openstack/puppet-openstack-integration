a0372071ba61692ddcbccd1b4b86ada25319317c
Takashi Kajinami <tkajinam@redhat.com>
2023-04-07 00:36:52 +0900

    Copy ovn ssl certs and keys to config directories
    Currently some AVC denials are appearing in audit.log, because neutron
    and octavia are not allowed to access to cert/key files in openvswitch
    directories. This change ensures these cert/key files are copied to
    individual config directories.
    
    Change-Id: I79b8603fe61e42d8444dc2fee77725434da9c69c

--- manifests/ovn.pp
+++ manifests/ovn.pp
@@ -10,8 +10,6 @@ class openstack_integration::ovn(
     class { 'vswitch::pki::cacert': }
     vswitch::pki::cert { ['ovnnb', 'ovnsb', 'ovncontroller']: }
 
-    $proto = 'ssl'
-
     $ovn_nb_db_ssl_key     = '/etc/openvswitch/ovnnb-privkey.pem'
     $ovn_nb_db_ssl_cert    = '/etc/openvswitch/ovnnb-cert.pem'
     $ovn_nb_db_ssl_ca_cert = '/var/lib/openvswitch/pki/switchca/cacert.pem'
@@ -23,31 +21,25 @@ class openstack_integration::ovn(
     $ovn_controller_ssl_cert    = '/etc/openvswitch/ovncontroller-cert.pem'
     $ovn_controller_ssl_ca_cert = '/var/lib/openvswitch/pki/switchca/cacert.pem'
 
-    # NOTE(tkajinam): ovn-pki generates a private key with 0600, owned by root
-    #                 but that does not allow access by ovn/neutron/octavia.
-    file { '/etc/openvswitch/ovnnb-privkey.pem':
-      ensure    => present,
-      mode      => '0644',
-      subscribe => Exec['ovs-req-and-sign-cert-ovnnb'],
-    }
-    file { '/etc/openvswitch/ovnsb-privkey.pem':
-      ensure    => present,
-      mode      => '0644',
-      subscribe => Exec['ovs-req-and-sign-cert-ovnsb'],
-    }
-    file { '/etc/openvswitch/ovncontroller-privkey.pem':
-      ensure    => present,
-      mode      => '0644',
-      subscribe => Exec['ovs-req-and-sign-cert-ovncontroller'],
+    ['ovnnb', 'ovnsb'].each |$ovndb| {
+      file { "/etc/openvswitch/${ovndb}-privkey.pem":
+        ensure  => present,
+        mode    => '0600',
+        owner   => 'openvswitch',
+        group   => 'openvswitch',
+        require => Vswitch::Pki::Cert[$ovndb],
+      } ~> Service['northd']
     }
 
-    File['/etc/openvswitch/ovnnb-privkey.pem'] -> Service['northd']
-    File['/etc/openvswitch/ovnsb-privkey.pem'] -> Service['northd']
-    File['/etc/openvswitch/ovncontroller-privkey.pem'] -> Service['controller']
+    file { '/etc/openvswitch/ovncontroller-privkey.pem':
+      ensure  => present,
+      mode    => '0600',
+      owner   => 'openvswitch',
+      group   => 'openvswitch',
+      require => Vswitch::Pki::Cert['ovncontroller'],
+    } ~> Service['controller']
 
   } else {
-    $proto = 'tcp'
-
     $ovn_nb_db_ssl_key     = undef
     $ovn_nb_db_ssl_cert    = undef
     $ovn_nb_db_ssl_ca_cert = undef
@@ -60,9 +52,6 @@ class openstack_integration::ovn(
     $ovn_controller_ssl_ca_cert = undef
   }
 
-  $ovn_nb_connection = "${proto}:${::openstack_integration::config::ip_for_url}:6641"
-  $ovn_sb_connection = "${proto}:${::openstack_integration::config::ip_for_url}:6642"
-
   class { 'ovn::northd':
     dbs_listen_ip         => $::openstack_integration::config::ip_for_url,
     ovn_nb_db_ssl_key     => $ovn_nb_db_ssl_key,
@@ -73,7 +62,7 @@ class openstack_integration::ovn(
     ovn_sb_db_ssl_ca_cert => $ovn_sb_db_ssl_ca_cert,
   }
   class { 'ovn::controller':
-    ovn_remote                 => $ovn_sb_connection,
+    ovn_remote                 => $::openstack_integration::config::ovn_sb_connection,
     ovn_encap_ip               => $::openstack_integration::config::host,
     ovn_bridge_mappings        => ['external:br-ex'],
     ovn_cms_options            => 'enable-chassis-as-gw',
