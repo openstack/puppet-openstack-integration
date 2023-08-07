8f624221ea56d8232ac47ebde0bfc8ad82e38cde
Takashi Kajinami <tkajinam@redhat.com>
2023-08-07 12:39:26 +0900

    Apache: Disable default mods
    We should load the required mods automatically instead of relying on
    the list of modules loaded by puppetlabs-apache by default.
    
    Change-Id: I8f60238ddb82c188dc2042ab38fedb3b0edbaef3

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -2,5 +2,8 @@ class openstack_integration::apache {
 
   include openstack_integration::params
   include openstack_integration::config
-  include apache
+  class { 'apache':
+    default_mods  => false,
+    default_vhost => false,
+  }
 }
