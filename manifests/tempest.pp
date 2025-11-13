09ff119cd16f6a5c5ef2e3aa18eff6947ac70622
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-11-14 01:30:46 +0900

    tempest: Cap share API version
    Due to pending migration to CentOS Stream 10 we are using old version
    of manila in CI. Cap the maximum API version to avoid failure caused
    by the newer (and unavailable) API version expected by manila-t-p.
    
    Change-Id: I9f9b905111d5d23cf4604fb58966a8f3e39b3183
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/tempest.pp
+++ manifests/tempest.pp
@@ -370,6 +370,8 @@ class openstack_integration::tempest (
     share_multitenancy_enabled        => false,
     share_enable_protocols            => [downcase($share_protocol)],
     share_capability_storage_protocol => $share_protocol,
+    # TODO(tkajinam): Remove this once we complete migration to c10s.
+    share_max_microversion            => '2.89',
     designate_nameservers             => ["${openstack_integration::config::ip_for_url}:5322"],
     metric_backends                   => $metric_backends,
   }
